00ce785b96e24725e0adfa6108bbbba162e907cd
Clifton <cliftongsam@gmail.com>
2021-03-31 15:57:55 -0400

    initial commit

---
+++ homepage.php
@@ -0,0 +1,50 @@
+<html>
+<head>
+    <style>
+        h1{
+            color: black;
+            font-size: 500%;
+            text-align: center;
+        }
+
+        h2{
+            color: black;
+            font-size: 200%;
+            text-align: center;
+        }
+
+        form{
+            color: darkblue;
+            text-align: left;
+            font-size: 150%;
+            text-align: center;
+        }
+
+    </style>
+
+    <body>
+<h1>HOMEPAGE</h1>
+<h2>SIGN UP / LOG IN</h2>
+
+<form action="login.php" method="post">
+    E-mail: <input type="text" name="email"><br>
+    Password: <input type="password" name="password"><br><br>
+    <input type="submit"><br><br>
+</form>
+<form action="Sign%20up.php" method="post">
+    Sign Up:<br><br>
+    <input type="submit" name="sign">
+
+</form>
+
+<h2 style=>GUEST</h2>
+
+<form action="Guest.php" method="post">
+    Name: <input type="Text" name="name"><br><br>
+    <input type="submit">
+</form>
+</body>
+</head>
+</html>
+
+
---
+++ index.php
@@ -0,0 +1,42 @@
+<html>
+<head>
+    <style>
+    h1 {
+        color:darkblue;
+        text-align: center;
+        font-family: "Adobe Gothic Std B";
+        font-size: 500%;
+        border: 2px solid powderblue;
+        padding: 30px;
+    }
+    h2 {
+        color: rgb(200, 150, 100);
+        font-family: "Adobe Devanagari";
+        font-size: 200%;
+        border: powderblue;
+        text-align: center;
+    }
+    p{
+        font-family:"Script MT Bold";
+        font-size: 150%;
+        text-align: center;
+    }
+
+    </style>
+</head>
+
+
+<body>
+    <h1>REVIEW CREATIONS</h1>
+    <h2>WHAT'S THE APP?</h2>
+    <p>The Review Creation app aims to provide feedback based on daily lives. People could follow other users (such as friends, family) and see the feedback made by them through this platform. The reviews are provided by an individual about a venue, area, hotel, restaurant, or movies etc. The main concept behind this is for a user to choose a place to visit or have dinner just
+        by looking at the suggestions or reviews provided by anyone.  </p>
+
+
+    <h3> For login or as Guest</h3>
+
+    <a href="http://localhost:63342/IAR/homepage.php">
+        <button>HERE</button>
+    </a>
+</body>
+</html>
