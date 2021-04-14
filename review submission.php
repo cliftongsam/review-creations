315ba47305d7dd0f68b398dcb70e8e10c75525da
Clifton <cliftongsam@gmail.com>
2021-04-14 16:25:01 -0400

    set seteee

--- review submission.php
+++ review submission.php
@@ -9,21 +9,21 @@
 <?php
 require('db.php');
 session_start();
+var_dump($_REQUEST);
 if (isset($_REQUEST['reviews'])) {
-    $username = $_SESSION['username'];
+    $username=$_SESSION['username'];
     $place = ($_POST['place']);
     $place = mysqli_real_escape_string($conn, $place);
     $reviews = ($_POST['reviews']);
     $reviews = mysqli_real_escape_string($conn, $reviews);
-    $datetime = date("Y-m-d H:i:s");
 
-    $query = "INSERT into `feedback` VALUES ('$username','$place', '$reviews', '$datetime')";
+    $query = "INSERT into `posts` VALUES (NULL ,'$username', '$place', '$reviews')";
     $result = mysqli_query($conn, $query);
-
+    var_dump($result);
     if ($result== TRUE) {
         echo "<div class='form'>
                   <h3>Thank you for the submission.</h3><br/>
-                  <p class='link'>Click here to <a href='dashboard.php'>homepage</a></p>
+                  <p class='link'>Click here to <a href='display-star-rating.php'>rate</a></p>
                   </div>";
 
     }
@@ -39,6 +39,7 @@ if (isset($_REQUEST['reviews'])) {
         Place you visited:
             <input type = "text" class="form" name="place" placeholder="Visited" required >
         <br>Describe your experience :<br>
+
         <input required type="text" name="reviews" placeholder="Enter your experience" >
         <br>
         <input <button type="submit" name="submit" value="SUBMIT"</button>
