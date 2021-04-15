900f0a78be853b7dd7d25e86b30ddca121ec5f0f
Jude Mampilly <judemampilly@gmail.com>
2021-04-15 06:30:59 -0400

    Styling

--- review submission.php
+++ review submission.php
@@ -30,22 +30,38 @@ if (isset($_REQUEST['reviews'])) {
 }else {
     ?>
     <br><br><br><center><h1>
-        <?php
+        <p><?php
     $username  = $_SESSION['username'];
-    echo "WELCOME $username";
-    ?>
+    echo "Username: $username";
+    ?></p>
         </h1></center>
     <br><br><br><br><br>
-    <center><h2>Submit your Review Here:</h2><br></center>
+    <center><h2 >Submit your Review Here:</h2><br></center>
     <center><form  class= "form" action="" method="post">
         Place you visited:
             <input type = "text" class="form" name="place" placeholder="Visited" required >
-        <br>Describe your experience :<br>
-
-        <input required type="text" name="reviews" placeholder="Enter your experience" >
-        <br>
-        <input <button type="submit" name="submit" value="SUBMIT"</button>
-        <p class="link"><a href="dashboard.php">Click to dashboard</a></p>
+        <p style="position: absolute;
+width: 267px;
+height: 34px;
+left: 282px;
+top: 375px;
+"><br>Describe your experience :<br></p>
+            <input required type="text" name="reviews" placeholder="Enter your experience" style="position: absolute;
+width: 386px;
+height: 141px;
+left: 518px;
+top: 375px;">
+        <input <button type="submit" name="submit" value="SUBMIT" style="position: absolute;
+width: 172px;
+height: 34px;
+left: 605px;
+top: 549px;
+"></button>
+        <p class="link" style="position: absolute;
+width: 213px;
+height: 29px;
+left: 584px;
+top: 652px;"><a href="dashboard.php">Click to dashboard</a></p>
     </form></center>
     <?php
 }
